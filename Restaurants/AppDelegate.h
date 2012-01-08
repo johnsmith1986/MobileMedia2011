5d2887922749ea805eda553deed229783a469d56
Dimitri Stancioff <dstancioff@gmail.com>
2012-01-08 18:02:06 -0500

    First Commit

---
+++ Restaurants/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Restaurants
+//
+//  Created by Dimitri Stancioff on 1/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Restaurants/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Restaurants
+//
+//  Created by Dimitri Stancioff on 1/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
